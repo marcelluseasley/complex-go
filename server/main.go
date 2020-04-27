18aaa39cdd00cd79826eb244d97c09543a335902
Marcellus Easley <marcellus.easley@gmail.com>
2020-04-27 04:21:50 -0400

    commented out unused code

--- server/main.go
+++ server/main.go
@@ -15,10 +15,10 @@ import (
 	"github.com/gofiber/fiber"
 )
 
-type Fibonacci struct {
-	num int `json:"number"`
-	result int `json:"result"`
-}
+// type Fibonacci struct {
+// 	num int `json:"number"`
+// 	result int `json:"result"`
+// }
 
 func main() {
 	PGUser := os.Getenv("PGUSER")
@@ -48,10 +48,10 @@ func main() {
 
 	app.Get("/values/all", func(c *fiber.Ctx) {
 		//get all values from postgress
-		rows, err := db.Query("SELECT * from fibo")
-		if err != nil {
-			log.Println(err)
-		}
+		// rows, err := db.Query("SELECT * from fibo")
+		// if err != nil {
+		// 	log.Println(err)
+		// }
 		c.Send("NOT IMPLEMENTED")
 		
 	})
