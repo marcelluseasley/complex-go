2a63c954aa55b8716624cf6bb0b60a44ab0ef70e
Marcellus Easley <marcellus.easley@gmail.com>
2020-04-27 04:17:38 -0400

    add back postgres

--- server/main.go
+++ server/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 
+	
+	_ "github.com/lib/pq"
 
+	"os"
 	"strconv"
 
 	"github.com/gofiber/cors"
 	"github.com/gofiber/fiber"
 )
 
+type Fibonacci struct {
+	num int `json:"number"`
+	result int `json:"result"`
+}
+
 func main() {
+	PGUser := os.Getenv("PGUSER")
+	PGHost := os.Getenv("PGHOST")
+	PGDatabase := os.Getenv("PGDATABASE")
+	PGPassword := os.Getenv("PGPASSWORD")
+	PGPort := os.Getenv("PGPORT")
 
-
-
-
+	connStr := fmt.Sprintf("user=%s host=%s dbname=%s password=%s port=%s sslmode=disable",PGUser, PGHost, PGDatabase, PGPassword, PGPort)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS fibo (number INT, factorial INT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	// Fiber route handlers
 
 	app := fiber.New()
@@ -25,19 +48,33 @@ func main() {
 
 	app.Get("/values/all", func(c *fiber.Ctx) {
 		//get all values from postgress
-		c.Send("all the values")
+		rows, err := db.Query("SELECT * from fibo")
+		if err != nil {
+			log.Println(err)
+		}
+		c.Send("NOT IMPLEMENTED")
+		
 	})
 
 	app.Post("/values", func(c *fiber.Ctx) {
 		index := c.FormValue("index")
+		log.Printf("index is %s", index)
 		i, _ := strconv.ParseInt(index, 10, 32)
 
 		if i > 40 {
 			c.Status(422).Send("Index too high")
 		}
+		log.Printf("i is %d", i)
+		result := fib(i)
+		log.Printf("result is %d", result)
+		
+		_, err := db.Exec(`INSERT INTO fibo (number, factorial) VALUES ($1, $2)`,i, result)
+		if err != nil {
+			log.Println(err)
+		}
 
 		
-		c.Send(fib(i))
+		c.Send(result)
 	})
 
 	app.Listen(5000)
